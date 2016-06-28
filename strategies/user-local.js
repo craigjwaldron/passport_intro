859874c36b96f93ad648d00a77971ac54aeaf88c
Craig <craigjwaldron@gmail.com>
2016-06-28 14:19:06 -0500

    fourth commit

--- strategies/user-local.js
+++ strategies/user-local.js
@@ -1,5 +1,6 @@
 var passport = require ('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var pg = require('pg');
 
 // require module
 var connection = require('../modules/connection');
@@ -13,6 +14,27 @@ passport.use('local', new LocalStrategy(
     usernameField: 'username'
   }, function(req, username, password, passportDone) { // passport done
       console.log('hit local strategy');
+
+      pg.connect(connection, function(err, client, pgDone){
+        if(err){
+          console.log(err);
+        }
+        client.query('SELECT * FROM users WHERE username = $1', [username], function(err, result) {
+          pgDone();
+
+          if(err){
+            console.log(err);
+          }else{
+            // found something
+            console.log(result.rows);
+            if(result.rows >= 1){
+              var passwordDB= result.rows[0].password;
+
+
+            }
+          }
+        });
+      });
   }
 ));
 
